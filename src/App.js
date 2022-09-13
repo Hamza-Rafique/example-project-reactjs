260c43458652d73485f16969be05f0df1c302b40
Hamza Rafique <hamza.rafique@sl2.studio>
2022-09-13 15:01:43 +0500

    basic setup of my own website

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hey! Visiter</h1>
+        <h2>We have some exited Product for you....</h2>
+        <h3>Please Drop a Message bellow.</h3>
+        <div className="links">
+          <a href="https://api.whatsapp.com/send?phone=9203107242237">
+            WhatsApp
+            <li className="fa fa-whatsapp"></li>
+          </a>
+
+          <a href="mailto:hamzarafique964@gamil.com?subject = Feedback&body = Message">
+            Email
+            <li className="fa fa-envelope"></li>
+          </a>
+          <a href="https://web.facebook.com/Vicky-Impex-109714995216107">
+            Facebook
+            <li className="fa fa-facebook"></li>
+          </a>
+          <a href="https://www.linkedin.com/in/hamza-rafique-mehar/">
+            linkedin
+            <li className="fa fa-linkedin"></li>
+
+          </a>
+        </div>
       </header>
     </div>
   );
